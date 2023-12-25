e3040b813cdcebc90f41ab6e7b64d89c39b64c9b
Ibrahim Kamal <ibrahim.kamal@smarteye.ai>
2023-12-25 11:52:54 +0200

    Remove MongoDB terraform configuration

--- terraform/mongodb.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_mongodb_account" "eshop-mongodb" {
-    name                = "eshop-mongodb"
-    resource_group_name = "eshop_resource_group"
-    location            = "westeurope"
-    offer               = "Standard"
-    kind                = "MongoDB"
-    version             = "3.6"
-}
